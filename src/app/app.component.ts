e05707869f32e2a3a1edfa519dbea13451e2cdc7
megha thakur <meghathakur15@gmail.com>
2018-12-12 10:46:12 +0530

    ocr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  clickme(){
+  alert('called app component from js')
+  Tesseract.recognize('assets/paytm-test.png')
+.then(function(result){
+    console.log(result)
+})
+  }
 }
